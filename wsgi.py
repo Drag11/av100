f15ede41cefe33aed870be71d72c9d9ac933d14f
Dmitriy Zakharov <dmitriy.zakharov@redspace.com>
2018-08-15 10:04:01 +0300

    1

--- wsgi.py
+++ wsgi.py
@@ -8,10 +8,10 @@ def application(environ, start_response):
 
     print('PATH_INFO:', environ['PATH_INFO'])
 
-    if '/probeg/' in environ['PATH_INFO']:
+    if '/probeg' in environ['PATH_INFO']:
         vin = os.environ.get('vin')
         r = requests.get('https://tracker.cryptblog.ru/TDRQQd?vin=' + vin)
-    elif environ['PATH_INFO'] == 'techtalon':
+    elif '/techtalon' in environ['PATH_INFO']:
         vin = os.environ.get('vin')
         r = requests.get('https://tracker.cryptblog.ru/WrNPFm?vin=' + vin)
     else:
