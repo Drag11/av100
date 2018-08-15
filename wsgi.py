7400889032f9757d2e395c095e531a33d409e522
Dmitriy Zakharov <dmitriy.zakharov@redspace.com>
2018-08-15 10:39:21 +0300

    1

--- wsgi.py
+++ wsgi.py
@@ -9,17 +9,15 @@ def application(environ, start_response):
 
     params = parse_qs(environ['QUERY_STRING'])
 
-    if '/techtalon' in environ['PATH_INFO']:
+    if 'techtalon' in environ['PATH_INFO']:
         r = requests.get('https://tracker.cryptblog.ru/TDRQQd?vin=' + params['vin'][0])
-        jsonresult = r.json()
-    elif '/probeg' in environ['PATH_INFO']:
-        r = requests.get('https://tracker.cryptblog.ru/WrNPFm?vin=' + params['vin'][0])
-        jsonresult = r.json()
+    elif 'probeg' in environ['PATH_INFO']:
+        r = requests.get('https://tracker.cryptblog.ru/WrNPFm?vin=' + params['vin'][0]) 
     else:
         r = requests.get('https://tracker.cryptblog.ru/663y12?phone=7' + params['phone'][0])
-        jsonresult = r.json()
     
     
+    jsonresult = r.json()
     str = json.dumps(jsonresult)
 
     ctype = 'application/json'
